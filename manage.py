2e231f2fcd575f1ad5f422c10f7c28aed06145d2
MANIKANDAN RAJARATHINAM <95955716+Manikandan-0303@users.noreply.github.com>
2024-01-31 01:07:58 +0530

    Update manage.py

--- manage.py
+++ manage.py
@@ -3,20 +3,24 @@
 import os
 import sys
 
-
 def main():
     """Run administrative tasks."""
+    # Set the DJANGO_SETTINGS_MODULE environment variable.
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Parkinsons_Disease_Prediction.settings')
     try:
+        # Import execute_from_command_line function from Django's management module.
         from django.core.management import execute_from_command_line
     except ImportError as exc:
+        # If Django is not installed or importable, raise ImportError with instructions.
         raise ImportError(
             "Couldn't import Django. Are you sure it's installed and "
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
-        ) from exc  # Move 'from exc' here
+        ) from exc
+    
+    # Call execute_from_command_line with command-line arguments.
     execute_from_command_line(sys.argv)
 
-
+# Check if the script is being run directly.
 if __name__ == '__main__':
     main()
